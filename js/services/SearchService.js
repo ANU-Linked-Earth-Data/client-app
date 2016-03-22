6cea10d450163692277bdb84a938f8c723b92109
Zhiduo Zhang <u5586932@anu.edu.au>
2016-03-22 22:51:49 +1100

    Fix search

--- js/services/SearchService.js
+++ js/services/SearchService.js
@@ -8,16 +8,16 @@ app.factory('SearchService', function($http){
     SearchService.result = function() {
         $http.defaults.useXDomain = true;
         
-        return $http.get('http://144.6.235.235/standalone/accidents.html')
+        return $http.get('http://144.6.235.235/standalone/accidents.rdf')
             .then(function success(res) {
                 // return the enveloped data
-                console.log("Response: " + res.data.status);
+                console.log("Response: " + res.data);
 
                 if (res.data.status > 200) {
                     return res.data.statusText;
                 } else {
-                    console.log("Getting response: " + res.data.status);
-                    return res.data.message;
+                    console.log("Getting response: " + res.data);
+                    return res.data;
                 }
             }, function failed(res){
                 alert(res);
