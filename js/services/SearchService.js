deb575e7bf038a975c4163d78f4201ac50d8c721
Sam Toyer <sam@qxcv.net>
2016-05-14 21:47:12 +1000

    Attempt to clean things up a bit

--- js/services/SearchService.js
+++ js/services/SearchService.js
@@ -4,11 +4,11 @@
 
 app.factory('SearchService', function($http){
     var SearchService = {};
-    var server = 'http://localhost:3030/landsat/query?query=';
+    var server = 'http://144.6.231.77/landsat/query?query=';
     var getAll = server + 'query=PREFIX+geo%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23%3E%0APREFIX+geosparql%3A+%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fgeosparql%23%3E%0APREFIX+geos%3A+%3Chttp%3A%2F%2Fwww.telegraphis.net%2Fontology%2Fgeography%2Fgeography%23%3E%0APREFIX+geosp%3A+%3Chttp%3A%2F%2Frdf.geospecies.org%2Font%2Fgeospecies%23%3E%0A%0ASELECT+%3Fsubject+%3FgeoSparql+%3FtimePeriod+%3Fimage%0AWHERE+%7B%0A++%3Fsubject+a+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23Observation%3E%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fwww.example.org%2FANU-LED%23imageData%3E+%3Fimage%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fgeosparql%23asWKT%3E+%3FgeoSparql%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fsdmx%2F2009%2Fdimension%23timePeriod%3E+%3FtimePeriod%0A%7D%0ALIMIT+25';
     var getFirstTimeStamp = server + 'query=SELECT+%3Fsubject+%3FtimePeriod+%0AWHERE+%7B%0A++%3Fsubject+a+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23Observation%3E%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fsdmx%2F2009%2Fdimension%23timePeriod%3E+%3FtimePeriod%0A%7D%0AORDER+BY+DESC(%3FtimePeriod)%0ALIMIT+1';
-    var getLastTimeStamp = 'http://localhost:3030/landsat/query?query=SELECT+%3Fsubject+%3FtimePeriod+%0AWHERE+%7B%0A++%3Fsubject+a+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23Observation%3E%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fsdmx%2F2009%2Fdimension%23timePeriod%3E+%3FtimePeriod%0A%7D%0AORDER+BY+ASC(%3FtimePeriod)%0ALIMIT+1'
-    var getDistinctTimeStamps = 'http://localhost:3030/landsat/query?query=SELECT+DISTINCT+%3FtimePeriod+%0AWHERE+%7B%0A++%3Fsubject+a+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23Observation%3E%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fsdmx%2F2009%2Fdimension%23timePeriod%3E+%3FtimePeriod%0A%7D%0AORDER+BY+DESC(%3FtimePeriod)'
+    var getLastTimeStamp = 'http://144.6.231.77/landsat/query?query=SELECT+%3Fsubject+%3FtimePeriod+%0AWHERE+%7B%0A++%3Fsubject+a+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23Observation%3E%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fsdmx%2F2009%2Fdimension%23timePeriod%3E+%3FtimePeriod%0A%7D%0AORDER+BY+ASC(%3FtimePeriod)%0ALIMIT+1'
+    var getDistinctTimeStamps = 'http://144.6.231.77/landsat/query?query=SELECT+DISTINCT+%3FtimePeriod+%0AWHERE+%7B%0A++%3Fsubject+a+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23Observation%3E%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fsdmx%2F2009%2Fdimension%23timePeriod%3E+%3FtimePeriod%0A%7D%0AORDER+BY+DESC(%3FtimePeriod)'
 
     SearchService.getAll = function(query) {
         $http.defaults.useXDomain = true;
@@ -33,7 +33,7 @@ app.factory('SearchService', function($http){
                 // return the enveloped data
 
                 if (res.data.status > 200) {
-                    return res.data.statusText;
+                    return res.statusText;
                 } else {
                     //console.log("Data: " + res.data.toString());
                     //console.log("Headers: " + res.data.head);
@@ -44,7 +44,7 @@ app.factory('SearchService', function($http){
                 alert(res);
             });
 
-        /*store.execute('LOAD <http://localhost:3030/landsat/>\
+        /*store.execute('LOAD <http://144.6.231.77/landsat/>\
          INTO GRAPH <lisp>', function(err){
          if(err) {
          /*var query = 'PREFIX foaf:<http://xmlns.com/foaf/0.1/> SELECT ?o \
@@ -53,7 +53,7 @@ app.factory('SearchService', function($http){
          // process results
          });
 
-         var query = "http://localhost:3030/dataset.html?tab=query&ds=%2Flandsat&query=PREFIX+geo%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23%3E%0APREFIX+geosparql%3A+%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fgeosparql%23%3E%0APREFIX+geos%3A+%3Chttp%3A%2F%2Fwww.telegraphis.net%2Fontology%2Fgeography%2Fgeography%23%3E%0APREFIX+geosp%3A+%3Chttp%3A%2F%2Frdf.geospecies.org%2Font%2Fgeospecies%23%3E%0A%0ASELECT+%3Fsubject+%3FgeoSparql+%3FtimePeriod+%3Fimage%0AWHERE+%7B%0A++%3Fsubject+a+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23Observation%3E%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fwww.example.org%2FANU-LED%23imageData%3E+%3Fimage%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fgeosparql%23asWKT%3E+%3FgeoSparql%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fsdmx%2F2009%2Fdimension%23timePeriod%3E+%3FtimePeriod%0A%7D%0ALIMIT+25";
+         var query = "http://144.6.231.77/dataset.html?tab=query&ds=%2Flandsat&query=PREFIX+geo%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23%3E%0APREFIX+geosparql%3A+%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fgeosparql%23%3E%0APREFIX+geos%3A+%3Chttp%3A%2F%2Fwww.telegraphis.net%2Fontology%2Fgeography%2Fgeography%23%3E%0APREFIX+geosp%3A+%3Chttp%3A%2F%2Frdf.geospecies.org%2Font%2Fgeospecies%23%3E%0A%0ASELECT+%3Fsubject+%3FgeoSparql+%3FtimePeriod+%3Fimage%0AWHERE+%7B%0A++%3Fsubject+a+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23Observation%3E%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fwww.example.org%2FANU-LED%23imageData%3E+%3Fimage%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fgeosparql%23asWKT%3E+%3FgeoSparql%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fsdmx%2F2009%2Fdimension%23timePeriod%3E+%3FtimePeriod%0A%7D%0ALIMIT+25";
 
          store.execute(query, function(err, results){
          if(!err) {
@@ -112,7 +112,7 @@ app.factory('SearchService', function($http){
             .then(function success(res) {
                 // return the enveloped data
 
-                if (res.data.status > 200) {
+                if (res.status > 200) {
                     return res.data.statusText;
                 } else {
                     return callback(res.data);
