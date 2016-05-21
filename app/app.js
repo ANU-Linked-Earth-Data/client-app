ac192b7bc75febdf8b1eba72aa8a78b4eda87961
Sam Toyer <sam@qxcv.net>
2016-05-21 12:47:51 +1000

    Move deps to Bower

--- app/app.js
+++ app/app.js
@@ -2,20 +2,11 @@
  * Created by Duo on 22-Mar-16.
  */
 
-var app = angular.module('LEDApp', ['ngRoute','uiGmapgoogle-maps','ngMaterial', 'ngMessages', 'rzModule'])
-    .config(function(uiGmapGoogleMapApiProvider) {
-        uiGmapGoogleMapApiProvider.configure({
-            key: 'AIzaSyDtKR_08r8JNQdxjLflpWqS3kSDF3fBBp8',
-            v: '3.23', //defaults to latest 3.X anyhow
-            libraries: 'weather,geometry,visualization'
-        });
-    });
-
-
-app.controller('rootController', function() {
-    
-});
-
-app.controller('AppCtrl', function() {
+var app = angular.module('LEDApp', [
+    'ngRoute',
+    'ngMessages',
+    'rzModule'
+]).controller('rootController', function() {
+}).controller('AppCtrl', function() {
     var self = this;
 });
--- app/routes.js
+++ app/routes.js
@@ -9,18 +9,6 @@ app.config(function($routeProvider){
             controller: "SearchController",
             controllerAs: "sc"
         }
-    ).when("/html/map.html",
-        {
-            templateUrl: "map.html",
-            controller: "rootController",
-            controllerAs: "rC"
-        }
-    ).when("/map.html",
-        {
-            templateUrl: "html/map.html",
-            controller: "GoogleMapController",
-            controllerAs: "gmc"
-        }
     ).otherwise({
         redirectTo: '/html/search.html',
         controller: "SearchController",
