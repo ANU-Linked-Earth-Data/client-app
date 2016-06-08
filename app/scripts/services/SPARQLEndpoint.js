6f18dfd4e6bb0c6ecc90fdba1e2ab6c958f2c570
Sam Toyer <sam@qxcv.net>
2016-06-08 21:15:03 +1000

    Begin work on new SPARQL API

---
+++ app/scripts/services/SPARQLEndpoint.js
@@ -0,0 +1,75 @@
+'use strict';
+
+angular.module('LEDApp')
+        .factory('SPARQLEndpoint', function($http, $q, config) {
+        var SPARQLEndpoint = {};
+
+        $http.defaults.useXDomain = true;
+
+        SPARQLEndpoint._urls = config['sparql-endpoints'];
+
+        // This method just finds a working endpoint URL by blasting describe
+        // queries at everything in the config['sparql-endpoints'] list.
+        SPARQLEndpoint._getURL = function() {
+            return $q(function(resolve, reject) {
+                if ('_url' in this) {
+                    resolve(this._url);
+                    return;
+                }
+                var nextURL = this._urls.shift();
+                if (nextURL === undefined) {
+                    reject("Couldn't find a working endpoint URL");
+                    return;
+                }
+
+
+                var req = {
+                    method: 'GET',
+                    url: nextURL,
+                    params: {
+                        query: 'DESCRIBE <>'
+                    },
+                    headers: {
+                        'Accept': 'application/sparql-results+json,*/*;q=0.9'
+                    }
+                };
+
+                $http(req).then(function() {
+                    // All good!
+                    this._url = nextURL;
+                    console.info('Selected endpoint: ' + this._url);
+                    resolve(this._url);
+                }.bind(this), function (reason) {
+                    console.warn('Endpoint ' + nextURL + ' down, status: ' + reason.status);
+                    this._getURL().then(resolve, reject);
+                }.bind(this));
+            }.bind(this));
+        }.bind(SPARQLEndpoint);
+
+        // This method actually runs a SPARQL query against the configured
+        // endpoint.
+        SPARQLEndpoint.query = function(queryString) {
+            return $q(function(resolve, reject) {
+                this._getURL().then(function(endpointURL) {
+                    var req = {
+                        method: 'GET',
+                        url: endpointURL,
+                        params: {
+                            query: queryString
+                        },
+                        headers: {
+                            'Accept': 'application/sparql-results+json,*/*;q=0.9'
+                        }
+                    };
+
+                    return $http(req);
+                }).then(function(response) {
+                    resolve(response.data);
+                }, function(reason){
+                    reject(reason);
+                });
+            }.bind(SPARQLEndpoint));
+        };
+
+        return SPARQLEndpoint;
+    });
--- app/scripts/services/SearchService.js
+++ app/scripts/services/SearchService.js
@@ -1,55 +1,18 @@
 'use strict';
 
 angular.module('LEDApp')
-        .factory('SearchService', function($http, config) {
+        .factory('SearchService', function($http, SPARQLEndpoint) {
     var SearchService = {};
-    var server = config['sparql-endpoints'][0] + '?query=';
 
-    var getFirstTimeStamp = server + 'SELECT+%3Fsubject+%3FtimePeriod+%0AWHERE+%7B%0A++%3Fsubject+a+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23Observation%3E%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fsdmx%2F2009%2Fdimension%23timePeriod%3E+%3FtimePeriod%0A%7D%0AORDER+BY+DESC(%3FtimePeriod)%0ALIMIT+1';
-    var getLastTimeStamp = server + 'SELECT+%3Fsubject+%3FtimePeriod+%0AWHERE+%7B%0A++%3Fsubject+a+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23Observation%3E%0A++++.+%3Fsubject+%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fsdmx%2F2009%2Fdimension%23timePeriod%3E+%3FtimePeriod%0A%7D%0AORDER+BY+ASC(%3FtimePeriod)%0ALIMIT+1';
-    var getDistinctTimeStamps = server + 'SELECT%20DISTINCT%20%3FtimePeriod%20%0AWHERE%20%7B%0A%20%20%3Fsubject%20a%20%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23Observation%3E%0A%20%20%20%20.%20%3Fsubject%20%3Chttp%3A%2F%2Fwww.example.org%2FANU-LED%23time%3E%20%3FtimePeriod%0A%7D%0AORDER%20BY%20DESC(%3FtimePeriod)';
+    var getDistinctTimeStamps = [
+        'SELECT DISTINCT ?timePeriod WHERE {',
+          '?subject a <http://purl.org/linked-data/cube#Observation> .',
+          '?subject <http://www.example.org/ANU-LED#time> ?timePeriod',
+        '} ORDER BY DESC(?timePeriod)'
+    ].join('\n');
 
-    SearchService.getAll = function(query) {
-        $http.defaults.useXDomain = true;
-
-        console.log("Getting query: " + server+query);
-
-        var req = {
-            method: 'GET',
-            url: server+query,
-            headers: {
-                'Accept': 'application/sparql-results+json,*/*;q=0.9'
-            }
-        };
-
-        return $http(req)
-            .then(function success(res) {
-                // return the enveloped data
-
-                if (res.data.status > 200) {
-                    return res.data.statusText;
-                } else {
-                    return res.data;
-                }
-            }, function failed(res){
-                console.error(res);
-            });
-    };
-
-    SearchService.getFirstTime = function() {
-        this.queryServer(getFirstTimeStamp).then(function (data) {
-            return data.results.bindings[0].timePeriod.value;
-        });
-    };
-    
-    SearchService.getLastTime = function() {
-        this.queryServer(getLastTimeStamp).then(function (data) {
-            return data.results.bindings[0].timePeriod.value;
-        });
-    };
-    
     SearchService.getDistinctTime = function() {
-        return this.queryServer(getDistinctTimeStamps, function(data){
+        return SPARQLEndpoint.query(getDistinctTimeStamps).then(function(data) {
             var retVal = [];
             for (var i in data.results.bindings){
                 var timeStamp = data.results.bindings[i].timePeriod.value;
@@ -58,35 +21,6 @@ angular.module('LEDApp')
 
             return retVal;
         });
-    };
-
-    SearchService.queryServer = function (url, callback) {
-        $http.defaults.useXDomain = true;
-
-        // console.log("Getting query: " + url);
-
-        var req = {
-            method: 'GET',
-
-            // TODO: Change this hardcoded query
-            url: url,
-            headers: {
-                'Accept': 'application/sparql-results+json,*/*;q=0.9'
-            }
-        };
-
-        return $http(req)
-            .then(function success(res) {
-                // return the enveloped data
-
-                if (res.data.status > 200) {
-                    return res.data.statusText;
-                } else {
-                    return callback(res.data);
-                }
-            }, function failed(res){
-                console.error(res);
-            });
     };
 
     return SearchService;
