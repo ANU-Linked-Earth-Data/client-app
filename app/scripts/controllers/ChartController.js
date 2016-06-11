0fa595bff955bdcd3300e0f1bf5f5f2d7b0892fc
Zhiduo Zhang <u5586932@anu.edu.au>
2016-06-11 04:41:05 -0700

    Chart Changes

--- app/scripts/controllers/ChartController.js
+++ app/scripts/controllers/ChartController.js
@@ -7,7 +7,7 @@ angular.module('LEDApp')
 
 
 
-        /* $scope.bar = {
+        $scope.bar = {
             title:"Test Chart",
             description:"Test Chart description",
             units: "m",
@@ -70,7 +70,7 @@ angular.module('LEDApp')
 //        icon:'fa-line-chart',
 //        tooltip:'Detailed time series'
 //      }
-        ]; */
+        ];
 
         $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
         $scope.series = ['Series A', 'Series B'];
